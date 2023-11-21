91bc64b9da05fc38c0ca0f014805aae221a602e7
Marl0nico <marlon.nicolalde@epn.edu.ec>
2023-11-21 08:12:52 -0500

    Ejercicio modificado

---
+++ src/Autos.java
@@ -0,0 +1,38 @@
+public class Autos {
+    //atributos
+    public String marca;
+    public String modelo;
+    public int año;
+    public String color;
+    //constructor
+    public Autos(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getAño() {
+        return año;
+    }
+    public void setAño(int año) {
+        this.año = año;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("*****Bienvenido*****");
+        Autos auto1=new Autos("Chevrolet", "Camaro", 2018, "Rojo");
+        Autos auto2=new Autos("Nissan", "Versa", 2022, "Blanco");
+        Autos auto3=new Autos("Toyota", "Hilux CD", 2020, "Negro");
+        System.out.println(auto1.color);
+        System.out.println(auto2.color);
     }
 }
