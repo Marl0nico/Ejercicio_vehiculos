b820db53c460d558af951acf4c45021dfb7b4218
Marl0nico <marlon.nicolalde@epn.edu.ec>
2023-11-21 09:05:17 -0500

    Ejercicio modificado

--- src/Autos.java
+++ src/Autos.java
@@ -5,7 +5,6 @@ public class Autos {
     public int año;
     public String color;
     //constructor
-<<<<<<< HEAD
     public Autos(String marca, String modelo, int año, String color) {
         this.marca = marca;
         this.modelo = modelo;
@@ -36,48 +35,7 @@ public class Autos {
     public void setColor(String color) {
         this.color = color;
     }
-}
-=======
-    public Autos (String nuevoNombre){
-        modelo=nuevoNombre;
-    }
-    //métodos
-    public int getAño(){
-        return año;
-    }
-    public String getModelo(){
-        return modelo;
-    }
-    public String getMarca(){
-        return marca;
-    }
-    public String getColor(){
-        return color;
-    }
-    //setters
-    public void setMarca(String newMarca){
-        this.marca =newMarca;
-    }
-    public void setModelo(String newModelo){
-        this.modelo =newModelo;
-    }
-    public void setColor(String newColor){
-        this.color=newColor;
-    }
-    public void setAño(int newAño){
-        this.año =newAño;
-        
-    }
-    
-    public void imprimirDatos(){
-        String temporalmarca = getMarca();
-        String temporalmodelo = getModelo();
-        int temporalaño = getAño();
-        String temporalcolor = getColor();
-        o
+    public void ImprimirDatos(){
+        System.out.println("Año:"+getAño()+"    Marca:"+getMarca()+"    Modelo:"+getModelo()+"  Color:"+getColor());
     }
 }
-
-
-
->>>>>>> origin/master
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("*****Bienvenido*****");
         Autos auto1=new Autos("Chevrolet", "Camaro", 2018, "Rojo");
         Autos auto2=new Autos("Nissan", "Versa", 2022, "Blanco");
         Autos auto3=new Autos("Toyota", "Hilux CD", 2020, "Negro");
-        System.out.println(auto1.color);
-        System.out.println(auto2.color);
+        auto3.ImprimirDatos();
+        auto2.ImprimirDatos();
+        auto1.ImprimirDatos();
+        System.out.println(auto1.getAño()+auto2.getAño()+auto3.getAño());
     }
 }
